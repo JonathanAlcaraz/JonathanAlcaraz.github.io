6048a5b55c78d84ddb353d63c48eca4509e90f79
JonathanAlcaraz <git@jonathanalcaraz.com>
2022-01-05 22:42:57 -0800

    updated CV and added email to home page

--- assets/email-obfuscator.js
+++ assets/email-obfuscator.js
@@ -1,1 +1,3 @@
-document.write('<'+'a'+' '+'h'+'r'+'e'+'f'+'='+"'"+'m'+'a'+'i'+'l'+'t'+'o'+':'+'s'+'i'+'t'+'e'+'@'+'j'+'o'+'n'+'a'+'t'+'h'+'a'+'n'+'a'+'l'+'c'+'a'+'r'+'a'+'z'+'.'+'c'+'o'+'m'+"'"+'>'+'Email'+'<'+'/'+'a'+'>');
+window.onload = function () {
+  document.getElementById("email").outerHTML="<n uers=\"znvygb:fvgr@wbangunanypnenm.pbz\">Rznvy</n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
+};
